2e95fa030d901d1cc04f9466fc9bf213011dd6aa
papajo <joshipv2@gmail.com>
2018-11-29 16:13:15 -0500

    define dockerfile, define db schema, routes

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,18 +1,30 @@
 # services/users/project/__init__.py
 
+import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-# instantiate the app
-app = Flask(__name__)
+# instantiate the db
+db = SQLAlchemy()
 
-# set config
-app.config.from_object('project.config.DevelopmentConfig')  #new
+# new
+def create_app(script_info=None):
+    # instantiate the app
+    app = Flask(__name__)
+    
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+    
+    # set up extensions
+    db.init_app(app)
+    
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-	'status': 'success',
-	'message': 'pong!'
-    })
-
- 
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+    return app
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,20 +1,25 @@
 # services/users/project/config.py
 
+import os
+
 class BaseConfig:
-      """Base Configuration"""
-      TESTING = False
+    """Base configuration"""
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'
 
 class DevelopmentConfig(BaseConfig):
       """Development Configuration"""
-      pass
+      SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
       """Testing Configuration"""
       TESTING = True
+      SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(BaseConfig):
       """Production Configuration"""
-      pass
+      SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
   
